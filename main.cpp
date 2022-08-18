bb7f684d5316e4ac4185e0e57b52cd9a1cd7bf5d
Robert Lissi <96295395+Robert9789@users.noreply.github.com>
2022-08-18 21:27:02 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,24 +1,26 @@
- 
+  
 #include<bits/stdc++.h> 
-#include <iostream>    
+#include <iostream>
 #include <dlt/dlt.h>
 
 // Limita maximă de bytes
 #define maxim 10000
-using namespace std; 
+ using namespace std;
   DLT_DECLARE_CONTEXT(con_example1);
-stdin();
-// Numele fișierului dat ca și argument pe linie
+//stdin();
+// Numele fișierului dat ca și comandă argument pe linie
 int main(int argc, char* argv[])
 {
     DLT_REGISTER_APP("Exemplu", "Primul exemplu");
     DLT_REGISTER_CONTEXT(con_example1, "CON", "First context");
+    DLT_LOG(con_example1, DLT_LOG_INFO, DLT_STRING("start input"));
+
     // Pointer la fișierul care urmează să fie citit 
     FILE* p;
     char chr;
-  // Stochează bytes pentru citire
-    char v[maxim];
     int i=0,inceput,final,j;
+    // Stochează bytes pentru citire
+      char v[maxim];
     // Dacă fișierul există și are permisiunea de citire
     p = fopen(argv[1], "r");
     if (p == NULL) 
@@ -29,8 +31,10 @@ int main(int argc, char* argv[])
     cout<<"Citim bytes de la: ";
     scanf("%d", &inceput);
 
-    cout<<"Citeste bytes până la: ";
+    cout<<"Citeste bytes până la: "<<endl;
     scanf("%d", &final);
+
+
 // For pentru a citi byte-ti din fișier
     for (i=0,j=0;i<= final && chr!=EOF;i++) 
     {
@@ -43,31 +47,31 @@ int main(int argc, char* argv[])
        // primeste caractere
         chr = fgetc(p);
     }
-
 // Afișează bytes ca șir de caractere
-	DLT_LOG(con_example1, DLT_LOG_INFO, DLT_STRING("Message nr "),DLT_INT32(i));
- cout<<v<<" ";
-    // inchiderea fișierului
-    fclose(p);
-    return 0;
+ cout<<v<<" "<<endl;
 
+ //alta varianta
+ // var input string
+// func main() {
+ //    f.Print("text=")
+  //   f.Scanf("%s", &input)
+ //   f.Println("Hello " + input)
+
+
+ // inchiderea fișierului
+    fclose(p);
+        DLT_LOG(con_example1, DLT_LOG_INFO, DLT_STRING("finish input"));
     DLT_UNREGISTER_CONTEXT(con_example1);
     DLT_UNREGISTER_APP();
 }
 
 
-   @version: 3.16
-log { 
-	source { stdin(); };
-	destination { file("/home/robert/stdout"); };
-};
-//alta varianta
- var input string
-func main() {
-    f.Print("text=")
-    f.Scanf("%s", &input)
-    f.Println("Hello " + input)
-}
+   //@version: 3.16
+//log {
+        //source { stdin(); };
+        //destination { file("/home/robert/stdout"); };
+//};
+ 
 //  Driverul stdin() colectează mesaje de pe fluxul de intrare standard.
 //  Atunci când fluxul de intrare standard este închis, 
 // syslog-ng se oprește, iar stdin() moștenește toate opțiunile de la sursa file(),
